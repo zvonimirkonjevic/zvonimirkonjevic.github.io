1da9b5395c16ebf8a7eac410261888e048e373bf
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-03-05 21:56:34 +0100

    implement custom favicon and switch of favicons when user changes theme

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,11 +1,37 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+
+function FaviconSwitcher() {
+    const { resolvedTheme } = useTheme();
+
+    React.useEffect(() => {
+        if (!resolvedTheme) return;
+
+        const isDark = resolvedTheme === "dark";
+        const iconPath = isDark ? "/icon-dark.png" : "/icon-light.png";
+
+        let link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+        if (!link) {
+            link = document.createElement("link");
+            link.rel = "icon";
+            document.head.appendChild(link);
+        }
+        link.href = iconPath;
+    }, [resolvedTheme]);
+
+    return null;
+}
 
 export function ThemeProvider({
     children,
     ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-    return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+    return (
+        <NextThemesProvider {...props}>
+            <FaviconSwitcher />
+            {children}
+        </NextThemesProvider>
+    );
 }
